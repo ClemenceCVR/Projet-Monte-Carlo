73856ae3c56d0397186ba383c9f329726d8569e3
Clemence Chevrier <clemence.chevrier@ensae.fr>
2024-04-10 16:47:54 +0200

    Question 1 : 1234

--- Projet Monte Carlo.py
+++ Projet Monte Carlo.py
@@ -2,4 +2,55 @@
 
 #Question 1 : Implement the generator for alpha-stable distributions proposed in Appendix B,and explain why it is valid.
 
-#essai
+import numpy as np 
+
+#On fixe la graine pour pouvoir reproduire les résultats 
+np.random.seed(123)
+
+#1 On échantillonne W à partir d'une distri exponentielle
+w = np.random.exponential(scale=1)
+print("w =", w)
+
+#2
+u = np.random.uniform(-np.pi/2, np.pi/2)
+print("u =", u)
+
+def S(alpha, beta): 
+    tan_term = np.tan(np.pi * alpha / 2)**2
+    result = (1 + beta**2 * tan_term)**(1 / (2 * alpha))
+    return result
+
+def B(alpha, beta):
+    tan_term = beta * np.tan(np.pi * alpha / 2)
+    result = 1 / alpha * np.arctan(tan_term)
+    return result
+
+def calculate_y_barre(alpha, beta, u, w):
+    if alpha != 1:
+        S_alpha_beta = S(alpha, beta)
+        B_alpha_beta = B(alpha, beta)
+        sin_alpha = np.sin(alpha)
+        cos_u = np.cos(u)
+        term1 = S_alpha_beta * (sin_alpha * (u + B_alpha_beta))
+        term2 = (cos_u) ** (1 / alpha)
+        term3 = np.cos(u - alpha * (u + B_alpha_beta)) / w
+        term4 = ((1 - alpha) / alpha)
+        y_barre = (term1 / term2) * (term3 ** term4)
+    else:
+        term1 = (np.pi / 2 + beta * u) * np.tan(u)
+        term2 = beta * np.log(np.pi / 2 * w * np.cos(u) / (np.pi / 2 + beta * u))
+        y_barre = 2 / np.pi * (term1 - term2)
+    return y_barre
+
+print(calculate_y_barre(0.6,1,u,w))
+
+#4
+
+def transformation(alpha, beta, gamma, delta, u, w):
+    y_barre = calculate_y_barre(alpha, beta, u, w)
+    y = gamma * y_barre + delta
+    return y
+
+
+
+
